5546e5040bc5490c47cbf423e0be2b4d1f6d5f5a
Farid Gasimov <farid.gasimov95@gmail.com>
2026-06-29 21:39:57 +0400

    chore: route comment

--- backend/src/routes/wordleRoutes.ts
+++ backend/src/routes/wordleRoutes.ts
@@ -9,6 +9,7 @@ import {
  * Route for Wordle
  * GET api/wordle/word - get random word.
  * POST api/wordle/stats - update game's global statistics.
+ * GET api/wordle/stats - get game's statistics for a specific mode and difficulty
  */
 const router = Router();
 
