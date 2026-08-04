2910b528edad1eaebd2b6d6b5f05231246daad07
Farid Gasimov <farid.gasimov95@gmail.com>
2026-08-04 21:01:22 +0400

    feat: added new route names for wordsmith

--- backend/src/routes/wordsmithRoutes.ts
+++ backend/src/routes/wordsmithRoutes.ts
@@ -1,14 +1,23 @@
 import { Router } from "express";
-import { fetchPuzzle, updateStats } from "../controllers/wordsmithController";
+import {
+  fetchEndlessDistribution,
+  fetchPuzzle,
+  fetchStats,
+  updateStats,
+} from "../controllers/wordsmithController";
 
 /**
  * Route for Wordsmith
- * GET api/wordsmith/puzzle - get random puzzle.
- * POST api/wordsmith/stats - update game's global statistics.
+ * GET api/wordle/word - get random puzzle.
+ * POST api/wordle/stats - update game's global statistics.
+ * GET api/wordle/stats - get game's statistics for a specific mode and difficulty
+ * GET api/wordle/distribution - get game's full stats for endless mode
  */
 const router = Router();
 
-router.get("/puzzle", fetchPuzzle);
+router.post("/word", fetchPuzzle);
 router.post("/stats", updateStats);
+router.get("/stats", fetchStats);
+router.get("/distribution", fetchEndlessDistribution);
 
 export default router;
