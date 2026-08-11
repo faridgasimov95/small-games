5e5223d1e28a3c75dab823d1bdeaf6b0ca8a4e53
Farid Gasimov <farid.gasimov95@gmail.com>
2026-08-11 20:24:32 +0400

    feat: fetching stats in wordsmith result modal and passing extra props to it

--- frontend/src/games/wordsmith/WordsmithResultModal.tsx
+++ frontend/src/games/wordsmith/WordsmithResultModal.tsx
@@ -1,35 +1,66 @@
-import type { DailyStats, EndlessStats } from "@/types/wordsmith";
-import { toDailyDistribution } from "./utils";
+import type {
+  DailyStats,
+  Difficulty,
+  EndlessStats,
+  Mode,
+} from "@/types/wordsmith";
+import { getBucketIndex, toDailyDistribution } from "./utils";
 import EndlessStatsView from "@/components/EndlessStatsView";
 import DailyStatsView from "@/components/DailyStatsView";
+import { useEffect, useState } from "react";
+import Modal from "@/components/Modal";
+const API_URL = import.meta.env.VITE_API_URL;
 
 type WorsmithResultModalProps = {
-  mode: "daily" | "endless";
+  mode: Mode;
+  difficulty: Difficulty;
   dailyStats?: DailyStats;
   endlessStats?: EndlessStats;
-  currentStreak?: number;
+  streak?: number;
+  completionTime: number;
   onClose: () => void;
 };
 
 export default function WordsmithResultModal({
   mode,
+  difficulty,
   dailyStats,
   endlessStats,
-  currentStreak,
+  streak,
+  completionTime,
   onClose,
 }: WorsmithResultModalProps) {
+  const [stats, setStats] = useState<DailyStats | EndlessStats | null>(null);
+
+  useEffect(() => {
+    const fetchStats = async () => {
+      const streakParam = mode === "endless" ? `&streak=${streak}` : "";
+      const response = await fetch(
+        `${API_URL}/wordsmith/stats?difficulty=${difficulty}&mode=${mode}${streakParam}`,
+      );
+      const data = await response.json();
+
+      setStats(data);
+    };
+
+    fetchStats();
+  }, [mode, difficulty, streak]);
+
   return (
-    <div>
-      {mode === "daily" && dailyStats && (
-        <DailyStatsView
-          {...toDailyDistribution(dailyStats.resultCounts)}
-          barCount={5}
-        />
-      )}
-      {mode === "endless" && endlessStats && currentStreak !== undefined && (
-        <EndlessStatsView stats={endlessStats} currentStreak={currentStreak} />
-      )}
-      <button onClick={onClose}>Close</button>
-    </div>
+    <Modal onClose={onClose}>
+      <div>
+        {stats && mode === "daily" && "resultCounts" in stats && (
+          <DailyStatsView
+            {...toDailyDistribution(stats.resultCounts)}
+            barCount={5}
+            highlightedBar={getBucketIndex(completionTime)}
+          />
+        )}
+        {stats && mode === "endless" && "percentile" in stats && (
+          <EndlessStatsView stats={stats} currentStreak={streak ?? 0} />
+        )}
+        <button onClick={onClose}>Close</button>
+      </div>
+    </Modal>
   );
 }
